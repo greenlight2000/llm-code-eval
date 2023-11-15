37455afc946c284ead04ee9671d5480a29b8272b
yunzhel2 <yunzhel2@illinois.edu>
2023-11-15 15:25:43 -0600

    scorer on ps, cr, ct

---
+++ scorers/score_program_synthesis.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+import pandas as pd
+import re
+import argparse
+import numpy as np
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--load_path', type=str, )
+    parser.add_argument('--output_path', type=str, )
+    args = parser.parse_args()
+    return args
+
+support_lang_clusters = ['C++', 'Java', 'Python', 'C', 'C#', 'Ruby',  'Go',
+                'Javascript', 'Kotlin', 'PHP', 'Rust',  'd', 'perl', 'delphi',]
+
+def valid_execfiles(exec_outcome_dir):
+    exec_outcome = pd.read_json(exec_outcome_dir, lines=True)
+    exec_outcome['lang_cluster'] = exec_outcome['lang_cluster'].apply(lambda x: x.lower())
+
+
+    def is_correct(row):
+        if all(item['exec_outcome'] == 'PASSED' for item in row['unittests']):
+            return 'True'
+        else:
+            return 'False'
+
+    exec_outcome['is_correct'] = exec_outcome.apply(is_correct, axis=1)
+
+    correct_outcome = exec_outcome[exec_outcome['is_correct'] == 'True']
+
+
+    for lang_cluster in support_lang_clusters:
+        exec_outcome_cur = exec_outcome[exec_outcome['lang_cluster'] == lang_cluster.lower()]
+        exec_num = exec_outcome_cur['src_uid'].drop_duplicates().shape[0]
+        correct_outcome_cur = correct_outcome[correct_outcome['lang_cluster'] == lang_cluster.lower()]
+        correct_num = correct_outcome_cur['src_uid'].drop_duplicates().shape[0]
+        print(f"Pass rate on {lang_cluster}: {correct_num/exec_num} ")
+
+    return exec_outcome
+
+def main():
+    processed_outcome = valid_execfiles(args.load_path)
+    # Save processed outcome result
+    processed_outcome.to_json(args.output_path,lines=True)
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main()
+
+
